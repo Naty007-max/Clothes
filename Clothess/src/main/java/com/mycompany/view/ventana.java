7f6608be35348b253dd3d0259b0d8cdc59afefd9
Naty007 <natalymesa61@gmail.com>
2026-07-13 10:18:44 -0500

    test ventana

---
+++ Clothess/src/main/java/com/mycompany/view/ventana.java
@@ -0,0 +1,11 @@
+package com.mycompany.view;
+
+import javax.swing.*;
+
+public class ventana {
+    private JComboBox cre;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
